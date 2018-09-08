90a4423a3ff2ce193c45c46fbac2245023f22ee1
Ayan banerjee <ayanb1999@gmail.com>
2018-09-08 23:31:30 +0530

    Setted up NavBar

---
+++ src/store/modules/auth.js
@@ -0,0 +1,18 @@
+const state ={
+    token: null
+};
+
+const getters = {
+    isLoggedIn: (state) => {
+        !!state.token
+    }
+};
+
+
+const actions = {
+    
+};
+
+const mutations = {
+
+}
