e564113e841ee9db470414e1dca3f78cbbb7e8c1
Ayanrocks <ayanb1999@gmail.com>
2018-10-02 13:30:25 +0530

    Added upload logic

--- src/store/modules/images.js
+++ src/store/modules/images.js
@@ -1,5 +1,5 @@
-
 import api from "../../api/imgur";
+import { router } from "../../main";
 
 const state = {
   images: []
@@ -12,10 +12,15 @@ const getters = {
 };
 
 const actions = {
-  async fetchImages({ rootState }) {
+  async fetchImages({ rootState, commit }) {
     const { token } = rootState.auth;
     const response = await api.fetchImages(token);
-
+    commit("setImages", response.data.data);
+  },
+  async uploadImages({ rootState }, images) {
+    const { token } = rootState.auth;
+    await api.uploadImages(images, token);
+    router.push("/");
   }
 };
 
@@ -23,4 +28,11 @@ const mutations = {
   setImages: (state, images) => {
     state.images = images;
   }
+};
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
 };
